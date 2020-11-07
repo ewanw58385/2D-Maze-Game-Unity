234c45a22eacb55889a730c7432ff9279f48663f
ewanw58385 <72386462+ewanw58385@users.noreply.github.com>
2020-11-07 20:14:34 +0000

    updated code

---
+++ Assets/Scripts/player_move.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_move : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xSpeed=0;
+        float ySpeed=0;
+
+        if (Input.GetKey("a") && Input.GetKey("s") && Input.GetKey("d") && Input.GetKey("w") == false)
+        {
+            xSpeed = 0f;
+            ySpeed = 0f;
+        }
+
+        if (Input.GetKey("a"))
+        {
+            // registers a key held down and returns true
+            xSpeed = -6f;     
+            anim.SetFloat("speed", xSpeed);                        
+        }
+
+        if (Input.GetKey("d"))
+        {
+            // registers a key held down and returns true
+            xSpeed = 6f;
+            anim.SetFloat("speed", xSpeed);
+        } 
+
+        if (Input.GetKey("s"))
+        {
+            ySpeed = -6f;
+            anim.SetFloat("speed", ySpeed);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            ySpeed = 6f;
+            anim.SetFloat("speed", ySpeed);
+        }
+        
+        transform.position += new Vector3(xSpeed * Time.deltaTime, ySpeed * Time.deltaTime, 0);
+    }
+    
+}
