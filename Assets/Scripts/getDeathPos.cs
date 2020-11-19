11cfcb4cef8502dcf7d9b21ae2b5acdbaf0a71b6
ewanw58385 <72386462+ewanw58385@users.noreply.github.com>
2020-11-19 15:32:27 +0000

    updated code

---
+++ Assets/Scripts/getDeathPos.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getDeathPos : MonoBehaviour
+{
+    private GameObject enemy;
+    public Vector2 deathLoc;
+
+    void Start()
+    {
+        enemy = GameObject.Find("enemy");
+    }
+
+    void Update()
+    {
+        Vector2 deathLoc = enemy.GetComponent<attackScript>().deathPos;
+        Debug.Log(deathLoc);
+    }
+
+}   
